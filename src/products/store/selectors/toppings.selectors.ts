00c2d388e6b8cfa36ec67add73e25d537a055173
Peter Reynolds <peterjrreynolds@gmail.com>
2018-07-11 14:25:06 +0100

    selector composition

---
+++ src/products/store/selectors/toppings.selectors.ts
@@ -0,0 +1,31 @@
+import { createSelector } from '@ngrx/store';
+
+import * as fromRoot from '../../../app/store';
+import * as fromFeature from '../reducers';
+import * as fromToppings from '../reducers/toppings.reducer';
+
+import { Topping } from '../../models/topping.model';
+
+export const getToppingsState = createSelector(
+  fromFeature.getProductsState,
+  (state: fromFeature.ProductsState) => state.toppings
+);
+
+export const getToppingEntities = createSelector(
+  getToppingsState,
+  fromToppings.getToppingEntities
+);
+
+export const getAllToppings = createSelector(getToppingEntities, entities => {
+  return Object.keys(entities).map(id => entities[parseInt(id, 10)]);
+});
+
+export const getToppingsLoaded = createSelector(
+  getToppingsState,
+  fromToppings.getToppingLoaded
+);
+
+export const getToppingsLoading = createSelector(
+  getToppingsState,
+  fromToppings.getToppingLoading
+);
