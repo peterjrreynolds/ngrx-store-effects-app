91505c0bb4f105d136eaa3ed246c30ebb59db615
Peter Reynolds <peterjrreynolds@gmail.com>
2018-07-11 13:50:45 +0100

    everything up to 11 toppings effects

---
+++ src/products/store/effects/pizzas.effect.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+import { Effect, Actions } from '@ngrx/effects';
+import { of } from 'rxjs/observable/of';
+import { switchMap, map, catchError } from 'rxjs/operators';
+
+import * as pizzasActions from '../actions/pizzas.action';
+import * as fromServices from '../../services';
+
+@Injectable()
+export class PizzasEffects {
+  constructor(
+    private actions$: Actions,
+    private pizzaService: fromServices.PizzasService
+  ) {}
+
+  @Effect()
+  loadPizzas$ = this.actions$.ofType(pizzasActions.LOAD_PIZZAS).pipe(
+    switchMap(() => {
+      return this.pizzaService.getPizzas().pipe(
+        map(pizzas => new pizzasActions.LoadPizzasSuccess(pizzas)),
+        catchError(error => of(new pizzasActions.LoadPizzasFail(error)))
+      );
+    })
+  );
+}
